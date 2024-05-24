2c87c9a3b70d844b8897150435b00c190aaeeee5
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-05-24 18:34:56 +0000

    jbk

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Calista.E
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  // this calculates volume of a sphere
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get('radius')
+  const bBase = params.get('bBase')
+  const height = params.get('height')
+  console.log(radius)
+
+  // process
+  const volume = Math.PI * (4.0 / 3.0) * Math.pow(radius, 3)
+  const dimensions = "<ul>\n<li>radius = " + "</li>\n</ul>"
+
+  // output
+  document.getElementById('dimensions').innerHTML = dimensions
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume + ' mm³'
+}
